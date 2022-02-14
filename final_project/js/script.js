9c21b98f3aeff4e71dd1b536d589761fd743079f
mngo118 <mngo@eastsideprep.org>
2022-02-14 10:59:05 -0800

    class work 2/14

--- final_project/js/script.js
+++ final_project/js/script.js
@@ -1,17 +1,25 @@
-// Source: https://coolcssanimation.com/how-to-trigger-a-css-animation-on-scroll/
-// TO DO: Figure out why this isn't working ;-;
+// Source: https://codepen.io/alvarotrigo/pen/PoKamZy 
 
-const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-        // If the element is visible
-        if (entry.isIntersecting) {
-            // Add the animation class
-            entry.target.classList.add('.appear');
+function reveal() {
+    // links with appear class in css (carries out animation)
+    var reveals = document.querySelectorAll(".appear");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        // get the space of top of element to top of document
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        // element visible determines what qualifies as "visible"
+        var elementVisible = 90;
+
+        // If the space above the element is less than the window height minus 150 (or in our case visable)
+        // Then add the active class (aka carry out animation)
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+            // If not then remove active class (keep it opacity 0)
+        } else {
+            reveals[i].classList.remove("active");
         }
-    });
-});
+    }
+}
 
-observer.observe(document.querySelector('.scroll-animation1'));
-observer.observe(document.querySelector('.scroll-animation2'));
-observer.observe(document.querySelector('.scroll-animation3'));
+window.addEventListener("scroll", reveal);
